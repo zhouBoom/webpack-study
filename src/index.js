327e6d6004f75f06328a672eaa0a8ed929fb107a
zhoubin6 <zhoubin6@tal.com>
2022-03-10 15:54:42 +0800

    feat: 基础篇完结

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import './main.css'
+import './main.css';
 import './sass.scss'
 import logo from '../public/avatar2.jpg'
-const a = 'hello world'
+
+const a = 'Hello ITEM'
 console.log(a)
 
 const img = new Image()
 img.src = logo
-document.getElementById('imgbox').appendChild(img)
-// module.export = a
+
+document.getElementById('imgBox').appendChild(img)
+
+// 新增装饰器的使用
+@log('hi')
+class MyClass { }
+
+function log(text) {
+  return function(target) {
+    target.prototype.logger = () => `${text}，${target.name}`
+  }
+}
+
+const test = new MyClass()
+test.logger()
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,63 @@
 // 最基本webpack配置
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 // 获取全局变量
-console.log('process.env.NODE_ENV=',process.env.NODE_ENV)
+console.log('process.env.NODE_ENV=', process.env.NODE_ENV)
 const config = {
     entry: './src/index.js', // 打包入口
-    mode: 'development', // 模式
+    mode: process.env.NODE_ENV, // 模式
     output: {
-        filename:'bundle.js', // 文件名
+        filename: 'bundle.js', // 文件名
         path: path.join(__dirname, 'dist') // 路径
     },
+    devtool: 'eval-cheap-module-source-map',
     module: {
-        rules:[
+        rules: [
             {
                 test: /\.(s[ac]|c)ss$/i, //匹配所有的css文件
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
             },
             {
-                test: /\.(jpe?g|png|gif)$/i, //匹配所有的css文件
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        name: '[name][hash:8].[ext]',
-                        limit: 200 * 1024  // 大于50k拷贝 小于50k则转为base64
+                test: /\.(jpe?g|png|gif)$/i,
+                type: 'asset',
+                generator: {
+                    // 输出文件位置以及文件名
+                    filename: '[name][hash:8][ext]'
+                },
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024 // 小于10k转为base64
                     }
-                }]
+                }
             },
+            { // webpack5自动url-loader、file-loader
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset',
+                generator: {
+                    // 输出文件位置以及文件名
+                    filename: '[name][hash:8][ext]'
+                },
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024 // 小于10k转为base64
+                    }
+                }
+            },
+            // babel
             {
-
+                test: /\.js$/i,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env'
+                            ],
+                        }
+                    }
+                ]
             }
         ]
     },
@@ -51,6 +79,6 @@ const config = {
 }
 
 module.exports = (env, argv) => {
-    console.log('argv.mode=',argv.mode) // 打印 mode(模式) 值
+    console.log('argv.mode=', argv.mode) // 打印 mode(模式) 值
     return config
 }
